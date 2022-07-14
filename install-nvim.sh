b93fd2e5ca235cfdcae664858786d994af73a4ca
Shutch <shutchinson1190@gmail.com>
2022-07-14 12:20:06 +1000

    Added basic neovim install with dotfile config

---
+++ install-nvim.sh
@@ -0,0 +1,19 @@
+apt-get update && \
+  apt-get install -y \
+  curl \
+  git
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb && \
+  apt install ./nvim-linux64.deb && \
+  rm -f ./nvim-linux64.deb
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+mkdir -p $HOME/.config/nvim && \
+  curl -L -o $HOME/.config/nvim/init.lua \
+  https://raw.githubusercontent.com/Shutch/dotfiles/master/nvim-init.lua
+
+nvim --headless -c 'PackerSync'
+
+echo "alias vim='nvim'" >> $HOME/.bashrc
